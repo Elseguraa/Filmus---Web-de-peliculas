46b4f22ce5e71e6c43429287cde6f03dd3d52d25
Elseguura <lucassegura2020@gmail.com>
2023-06-04 19:19:46 -0300

    terminado excepto media de contacto

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const peliculas = document.querySelectorAll('.pelicula');
 const flechaIzquierda = document.getElementById('flecha-izquierda');
 const flechaDerecha = document.getElementById('flecha-derecha');
 
-// ? ----- ----- Event Listener para la flecha derecha. ----- -----
+// ? ----- -----  para la flecha derecha. ----- -----
 flechaDerecha.addEventListener('click', () => {
 	fila.scrollLeft += fila.offsetWidth;
 
 	
 });
 
-// ? ----- ----- Event Listener para la flecha izquierda. ----- -----
+// ? ----- -----  para la flecha izquierda. ----- -----
 flechaIzquierda.addEventListener('click', () => {
 	fila.scrollLeft -= fila.offsetWidth;
 
